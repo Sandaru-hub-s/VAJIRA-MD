96d98d2c0d4d664bc1ad663a8f2c6345978bac59
Sandaru-hub-s <Sandarusamod1234567@gmail.com>
2025-07-02 20:16:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'https://github.com/VajiraOfficialBot/VAJIRA-MD' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=3s4hlQTK#sD-BBqvH2O0V1B0joSjTFzdrz4C7yafPZE5pPVMcAfk' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94719199757' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
